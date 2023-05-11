d05e9c061a4a14b8d36ae4db1f2aede6ebade035
alejandra solano <alejandrasol89023@gmail.com>
2023-05-10 21:46:03 -0500

    mi primer commit

---
+++ roles.frm.php
@@ -0,0 +1,21 @@
+<?php include_once "header.php"; ?>
+<div class="row my-5">
+<h1 class="text-center">formulario roles</h1>
+</div>
+
+<div class="row>">
+    <div class="col-3 d-flex justify-content-end aling-items-center">
+    <span class="fw-bolder">Nombre Rol:</span>
+    </div>
+
+    <div class="col-7">
+  <div class="form-floating">
+  <input type="text" class="form-control" id="floatingInput" placeholder="admin">
+  <label for="floatingInput">Nombre roles</label>
+</div>
+</div>
+</div>
+<div class="row mt-5 bg-dark">
+<h1 class="text-white text-center">tabla de roles</h1>
+</div>
+<?php include_once "footer.php"; ?>
